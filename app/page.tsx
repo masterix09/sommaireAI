3c5811ef75edbe55dedd339fcfc48e7a2d6834df
masterix09 <andrea.verde.1999@gmail.com>
2025-03-14 10:45:45 +0100

    finish to implement header and init to implement hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows=[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Sommaire</h1>
-      <Button variant={"outline"} size={"lg"}>
-        Upload PDF
-      </Button>
+    <div className="relative w-full ">
+      <HeroSection />
+      {/* <DemoSecton />
+      <HowItWorksSection />
+      <PricingSection />
+      <CTASection /> */}
     </div>
   );
 }
