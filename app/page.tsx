7425b63a7a58edfde4dc76eec1c8195265503660
masterix09 <andrea.verde.1999@gmail.com>
2025-03-14 13:35:57 +0100

    finish to implement hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import BGGradient from "@/components/common/bg-gradient";
 import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
     <div className="relative w-full ">
-      <HeroSection />
+      <BGGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
       {/* <DemoSecton />
       <HowItWorksSection />
       <PricingSection />
